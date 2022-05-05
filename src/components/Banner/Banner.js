b1901876377bee3225a7691f77ca6e16cc4a3e8d
Khoa Nguyen <khoa.nguyen@codestringers.com>
2022-05-06 02:00:53 +0700

    change logo

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,18 +1,19 @@
 import { Text, View } from 'native-base';
 import React from 'react';
 import { ImageBackground, StyleSheet } from 'react-native';
+export const IMAGENAME = require('../../../assets/images/logo.png'); 
 
 const styles = StyleSheet.create({
   root: {},
   image: {},
   text: {
-    color: 'white',
+    // color: 'white',
     fontSize: 24,
     fontWeight: '700',
   },
   innerContainer: {
     flex: 1,
-    backgroundColor: 'rgba(0,0,0, 1)',
+    // backgroundColor: 'rgba(0,0,0, 1)',
     borderRadius: 5
   },
 });
@@ -23,12 +24,13 @@ function Banner(props) {
   };
   return (
     <ImageBackground
-      source={image}
-      style={{ width: '100%', height: '100%', opacity: 0.5 }}
+      // source={image}
+      source={ IMAGENAME } 
+      style={{ width: '100%', height: '100%', opacity: 1 }}
       imageStyle={{ borderRadius: 5}}
 
     >
-      <View style={styles.innerContainer}>
+      {/* <View style={styles.innerContainer}> */}
         {/* <View
           style={{
             width: '100%',
@@ -40,7 +42,7 @@ function Banner(props) {
         >
           <Text style={styles.text}>Centered text</Text>
         </View> */}
-      </View>
+      {/* </View> */}
     </ImageBackground>
   );
 }
